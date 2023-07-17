77fd20a7b7c7b5eb70802840c3d52ae63bfa8350
Maaz-07 <maazchoudhry07@gmail.com>
2023-07-17 13:09:39 +0500

    Skills

--- typing-animation.js
+++ typing-animation.js
@@ -1,26 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const text = [
-    "I'm Maaz, a zealous programmer with a love for coding and problem-solving. I enjoy working on diverse projects and expanding my skills in various technologies.",
-    "I am currently a dedicated and passionate student at Fast-NU with a strong interest in programming and technology. As an aspiring developer, I am eager to learn and explore various programming languages and tools.",
-  ];
+function typeText(element, text, speed) {
+  let index = 0;
 
-  const typingAnimation = document.getElementById("typing-animation");
-  let paragraphIndex = 0;
-  let characterIndex = 0;
-
-  function typeText() {
-    const currentParagraph = text[paragraphIndex];
-
-    if (characterIndex <= currentParagraph.length) {
-      typingAnimation.innerHTML = currentParagraph.substring(0, characterIndex);
-      characterIndex++;
-      setTimeout(typeText, 40);
-    } else if (paragraphIndex < text.length - 1) {
-      paragraphIndex++;
-      characterIndex = 0;
-      typingAnimation.innerHTML += "<br><br>";
-      setTimeout(typeText, 40);
+  function type() {
+    if (index < text.length) {
+      element.textContent += text[index];
+      index++;
+      setTimeout(type, speed);
     }
   }
-  typeText();
+
+  type();
+}
+
+function typeMultipleTexts(elements, speed, delay) {
+  elements.forEach((element, index) => {
+    setTimeout(() => {
+      typeText(element, element.getAttribute("data-text"), speed);
+    }, index * delay);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const elements = document.querySelectorAll(".typing-animation");
+
+  typeMultipleTexts(elements, 40, 8000);
+
+  const aboutSection = document.querySelector(".fade-in");
+  aboutSection.classList.add("visible");
 });
